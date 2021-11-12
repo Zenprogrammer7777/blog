82e6c60730aec6fb837f725f6ea9201c5fa74742
dogeTheCoder <anthonyalfredo73@gmail.com>
2021-11-12 08:57:17 -0600

    added activerecord-import

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 
 export default (
-  <Home>
-    
-  </Home>
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  </Router>
 );
